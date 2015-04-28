066b72f8ab87b68248a3a19bc357fcf87a02719a
Anna <anna.klimas@gmail.com>
2015-04-28 16:29:01 +0100

    Final refactored fizzbuzz

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,10 +1,23 @@
 def fizzbuzz(number)
- return 'fizzbuzz' if div_by(number,15)
- return 'fizz'if div_by(number,3)
- return 'buzz' if div_by(number,5)
+ return 'fizzbuzz' if div_by_15(number)
+ return 'fizz'if div_by_3(number)
+ return 'buzz' if div_by_5(number)
  return number
 end
 
  def div_by(number,divisor)
   number % divisor == 0
  end
+
+def div_by_15(number)
+  div_by(number,15)
+end
+
+def div_by_3(number)
+  div_by(number, 3)
+end
+
+def div_by_5(number)
+  div_by(number,5)
+end
+
