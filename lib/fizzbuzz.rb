57a37ba61bb451205d7b283f5adea6abb8b80a27
Anna <anna.klimas@gmail.com>
2015-04-28 16:17:56 +0100

    Refactored fizzbuzz program

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,2 +1,10 @@
-def fizzbuzz
+def fizzbuzz(number)
+ return 'fizzbuzz' if div_by(number,15)
+ return 'fizz'if div_by(number,3)
+ return 'buzz' if div_by(number,5)
+ return number
 end
+
+ def div_by(number,divisor)
+  number % divisor == 0
+ end
